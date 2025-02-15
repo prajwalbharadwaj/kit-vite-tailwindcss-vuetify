94ac3ff902aab82110447a6f7fcb3602427ea98b
prajwalbharadwaj <prajwalrb17@gmail.com>
2025-02-15 22:19:24 +0530

    Added support for vuetify

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
 import { createPinia } from 'pinia';
-
 import { createApp } from 'vue';
-import App from './App.vue';
+import { createVuetify } from 'vuetify';
 
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+import App from './App.vue';
 import router from './router';
+
 import './assets/main.css';
+import 'vuetify/styles';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(vuetify);
 
 app.mount('#app');
