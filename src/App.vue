5f4a103e4326a9206dbb73c82c6ece617049c302
prajwalbharadwaj <prajwalrb17@gmail.com>
2025-02-15 21:35:00 +0530

    Added support for dark theme

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <script setup>
-import { computed } from 'vue';
-import DefaultLayout from '~/common/layouts/DefaultLayout.vue';
-import AuthLayout from '~/common/layouts/AuthLayout.vue';
-
+import { useDark } from '@vueuse/core';
+import { computed, onMounted, watch } from 'vue';
 import { useCommonImports } from '~/common/composables/CommonImports.composable.js';
+import AuthLayout from '~/common/layouts/AuthLayout.vue';
+import DefaultLayout from '~/common/layouts/DefaultLayout.vue';
 
 const { route } = useCommonImports();
+const isDark = useDark();
 
-const layout = computed(() => {
-  if (route?.meta?.layout === 'auth')
-    return AuthLayout;
-  else
-    return DefaultLayout;
-});
+const is_layout = computed(() => route?.meta?.layout === 'auth' ? AuthLayout : DefaultLayout);
+
+function setTheme() {
+  const html = document.getElementsByTagName('html')[0];
+
+  isDark.value
+    ? html.setAttribute('data-theme', 'dark')
+    : html.removeAttribute('data-theme'); ;
+}
+
+onMounted(() => setTheme());
+
+watch(isDark, (newValue, oldValue) => {
+  if (newValue !== oldValue)
+    setTheme();
+}, { immediate: false });
 </script>
 
 <template>
-  <component :is="layout" />
+  <component :is="is_layout" />
 </template>
--- src/common/components/Navbar.vue
+++ src/common/components/Navbar.vue
@@ -1,6 +1,4 @@
 <script setup>
-
-
 // function setTheme() {
 //   if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches))
 //     document.documentElement.classList.add('dark');
@@ -17,12 +15,18 @@
 //   setTheme();
 // });
 import { useDark, useToggle } from '@vueuse/core';
+
 const isDark = useDark();
 const toggleDark = useToggle(isDark);
 </script>
 
 <template>
-  <div>
-    <div @click="toggleDark()">button</div>
+  <div class="flex items-center justify-between shadow px-4 py-3 bg-white dark:bg-gray-900">
+    <div>
+      Starter Kit
+    </div>
+    <div @click="toggleDark()">
+      Change theme
+    </div>
   </div>
 </template>
